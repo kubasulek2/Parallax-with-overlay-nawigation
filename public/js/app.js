2e1e69860c54224e452fd3bfa5782a9d25451acd
Kuba Sukowski <kubasulek2@gmail.com>
2018-12-20 18:07:07 +0100

    title fixed animation finished

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
-$(()=>{});
+$(()=>{const e=$("#title");$("#header");let o=0;e.offset().top,e.height();$(document).on("scroll",function(){$(this).scrollTop();let t=$(this).scrollTop();t>o?e.addClass("pinned"):e.removeClass("pinned"),o=t})});
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,29 @@
 $( () => {
+  let scrollPosition;
+  const title = $('#title');
+  const container = $('#header');
+  let lastScrollTop = 0;
+  let titleTop = title.offset().top + title.height()/2;
+
+
+
+  function scroll() {
+    let scrollTop = $(this).scrollTop();
+    let st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+      // downscroll code
+      title.addClass("pinned")
+    } else{
+      title.removeClass("pinned")
+
+    }
+    lastScrollTop = st;
+
+  }
+
+
+  $(document).on('scroll',scroll)
+
+
 
 });
